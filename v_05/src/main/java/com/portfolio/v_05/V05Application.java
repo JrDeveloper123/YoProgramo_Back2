b08559ea1efbd095e3fca64fd134c0fa9aafd86d
Adrianclub <elratonpereznomiente@gmail.com>
2023-02-21 08:50:42 -0300

    prueba render

---
+++ v_05/src/main/java/com/portfolio/v_05/V05Application.java
@@ -0,0 +1,13 @@
+package com.portfolio.v_05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(V05Application.class, args);
+	}
+
+}
