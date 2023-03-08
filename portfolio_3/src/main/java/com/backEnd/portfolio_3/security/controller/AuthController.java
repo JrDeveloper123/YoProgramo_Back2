7ac23d91ad8f6516bf7023a5ed0336c4d994f38c
Adrianclub <elratonpereznomiente@gmail.com>
2023-03-08 20:42:58 -0300

    commit2

--- portfolio_3/src/main/java/com/backEnd/portfolio_3/security/controller/AuthController.java
+++ portfolio_3/src/main/java/com/backEnd/portfolio_3/security/controller/AuthController.java
@@ -35,7 +35,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"http://portfolioyoprog.web.app"})
+@CrossOrigin(origins = {"https://portfolioyoprog.web.app"})
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
