9c6f88ea20508aa92be06184c33ca952e03286d2
Adrianclub <elratonpereznomiente@gmail.com>
2023-03-14 16:35:09 -0300

    cors 222

--- portfolio_3/src/main/java/com/backEnd/portfolio_3/security/controller/AuthController.java
+++ portfolio_3/src/main/java/com/backEnd/portfolio_3/security/controller/AuthController.java
@@ -32,7 +32,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+/*
+*/
 
 @CrossOrigin(origins= "https://portfolioarch-3a99e.web.app")
 @RestController
